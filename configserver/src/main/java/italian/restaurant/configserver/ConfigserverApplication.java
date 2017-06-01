fa8301d34f3ab274ce5728a2b4c6bc41a4a10064
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-06-01 13:10:30 +0200

    Add admin server

--- configserver/src/main/java/italian/restaurant/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/italian/restaurant/configserver/ConfigserverApplication.java
@@ -2,11 +2,15 @@ package italian.restaurant.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverApplication.class, args);
+    }
 }
